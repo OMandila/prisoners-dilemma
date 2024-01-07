3751e343bb692ad7667a2653f30f4b6f760d5ccf
unknown <olivermandila@gmail.com>
2024-01-07 16:07:36 +0100

    feat: define global variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,24 @@ function typeIntroText() {
     // Start the typing effect
     addCharacter();
 }
+
+// Initialize global variables to track wins, losses, tries, and maximum number of tries.
+let wins = 0;
+let losses = 0;
+let tries = 0;
+let maxTries;
+let lastPlayerDecision;
+
+// Statistics object to track various game metrics.
+let stats = {
+    cooperations: 0,
+    defections: 0,
+    wins: 0,
+    losses: 0,
+    mutualBenefit: 0,
+    mutualLoss: 0
+};
+
+// Variable to hold the Chart.js chart instance.
+let statsChart;
+
